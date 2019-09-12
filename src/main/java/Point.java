75da43415b7a97a52dcf34a1ae10af72205fa29d
Cian Duffy <ciaduffy@cisco.com>
2019-09-12 19:02:53 +0100

    Adding point Class

---
+++ src/main/java/Point.java
@@ -0,0 +1,13 @@
+public class Point {
+    public double x;
+    public double y;
+    public double z;
+    public double angle;
+
+    public Point(double x, double y, double z, double angle) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.angle = angle;
+    }
+}
